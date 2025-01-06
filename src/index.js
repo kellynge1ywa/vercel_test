03f9f8baa3012d036a30207561dd92f7774a1304
Kelvin Ngeiywa <kellyngeiywa@gmail.com>
2025-01-06 15:41:55 +0300

    Vercel test

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+
+
+//initialize express
+const app= express();
+
+const PORT= process.env.PORT;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// app.use('/api', lipaNaMpesaRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to vercel deployment test!!');
+});
+
+app.listen(PORT, ()=>{
+    console.log(`The app is running on port: http://localhost:${PORT}`);
+})
+
+
+
+
+
